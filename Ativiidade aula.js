9035fd8830000e8f114ebc5cc785adeb32910055
GSBergerr <gabrielberger02@gmail.com>
2024-06-17 22:38:08 +0000

    teste

---
+++ Ativiidade aula.js
@@ -0,0 +1,15 @@
+let numeros = new Array(1,2,3,4,5)
+
+console.log(numeros[2]);
+
+let Pares = numeros.filter(function(numeros){
+    return numeros % 2 === 0;
+});
+
+function multiplicaPorDois(numeros) {
+    let dobro = [];           
+    for (let i of numeros) {
+      dobro.push(i * 2);
+    }
+    return numeros;   
+  }
